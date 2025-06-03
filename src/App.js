960ccc25c3383da18068eef9732ba503066f1eb9
admoisesdev <moi.prado@duocuc.cl>
2025-06-03 16:34:28 -0400

    git commit -m "feat: Añadir backend de autenticacion con serverless functions de netlify"

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PatientGrid from './components/patient_grid_component.tsx';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <PatientGrid />
+    </div>
+  );
+}
+
+export default App;
